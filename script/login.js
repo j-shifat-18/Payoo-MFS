cbdf45936966197319cd499232ab996077da2e2b
j-shifat-18 <info.jahirulsifat@gmail.com>
2025-02-25 12:45:21 +0600

    add index.html and main.html file ,and done login form and main file's nav , service section

---
+++ script/login.js
@@ -0,0 +1,21 @@
+document.getElementById('login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const mobileNumber = document.getElementById('mobile-number').value;
+    const pin = document.getElementById('pin').value;
+    
+    if(mobileNumber.length === 11){
+        if(pin === "1234"){
+            window.location.href = "./main.html";
+        }
+        else {
+            alert("Invalid Pin");
+        }
+    }
+    else{
+        alert("Need valid mobile number");
+    }
+
+
+
+});
